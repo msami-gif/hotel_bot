517565f97c217122ee332ee1719d583e9980b7a5
MSami <MSami@retrorabbit.co.za>
2025-08-06 14:15:44 +0200

    removed comments

--- main.py
+++ main.py
@@ -84,7 +84,6 @@ def search_and_display_results():
         price = o.get('price', {}).get('total', 'N/A')
         currency = o.get('price', {}).get('currency', '')
         availability = "Available" if hotel.get('available', True) else "Not available"
-        # policies = o.get('policies', {}).get('cancellations')[0].get('description').get('text', 'No cancellation policy available')
         cancellations = o.get('policies', {}).get('cancellations', [])
         if cancellations and 'description' in cancellations[0]:
             policies = cancellations[0]['description'].get('text', 'No cancellation policy available')
