bb219e6df56807ceadcb2cf8254f28d414a9d05d
MSami <MSami@retrorabbit.co.za>
2025-08-06 13:40:22 +0200

    Updated logic for search and display: Added availibility and policies

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def ask_for_missing_info():
     return f"Please provide: {', '.join(missing)}."
 
 def search_and_display_results():
-    print("\nSearching for hotels...")
+    print(f"\nSearching for hotels in {state["destination"]}...")
     hotels = hotel_service.search_hotels_by_city(
         city_code=city_name_to_code(state["destination"]),
         check_in_date=state["check_in_date"],
@@ -75,17 +75,20 @@ def search_and_display_results():
     if not hotels:
         return "Sorry, no hotels found."
 
-    reply = "\nHere are some hotel options:\n"
-    for hotel in hotels[:3]:
-        print(hotels)
+    reply = "\nHere are some hotel options:\n\n"
+    for hotel in hotels[:10]:
+        print(f"Available" if hotel.get('available', True) else "Not available")
         h = hotel["hotel"]
         o = hotel["offers"][0]
+        print(f"Offers: {hotel['offers']}")
         # reply += f"- {h['name']}, {h['address']['lines'][0]} – {o['price']['total']} {o['price']['currency']}\n"
         name = h.get('name', 'Unnamed Hotel')
         address = h.get('address', {}).get('lines', ['Address not available'])[0]
         price = o.get('price', {}).get('total', 'N/A')
         currency = o.get('price', {}).get('currency', '')
-        reply += f"- {name}, {address} – {price} {currency}\n"
+        availability = "Available" if hotel.get('available', True) else "Not available"
+        policies = o.get('policies', {}).get('cancellations')[0].get('description').get('text', 'No cancellation policy available')
+        reply += f" Hotel: {name}, Address: {address} – \n Price: {price} {currency} \n Availibility: {availability} \n Policy: {policies}\n\n"
     return reply
 
 def city_name_to_code(name):
@@ -93,8 +96,7 @@ def city_name_to_code(name):
     mapping = {
         "cape town": "CPT",
         "johannesburg": "JNB",
-        "durban": "DUR",
-        "london": "LON"
+        "durban": "DUR"
     }
     return mapping.get(name.lower(), name.upper())  # fallback
 
