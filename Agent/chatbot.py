fa20025f50329d37b608e2b59ba1ff2afaee4905
MSami <MSami@retrorabbit.co.za>
2025-08-06 09:41:55 +0200

    chatbot: Loading of model. Giving instructions to the model via Template of instructions. Extracting booking info from the prompt and formatting response.

---
+++ Agent/chatbot.py
@@ -0,0 +1,36 @@
+# llm_extraction.py
+import re
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+import json
+
+# Load model
+model = OllamaLLM(model="llama3")
+
+# Load prompt
+with open("Template/template.txt", "r", encoding="utf-8") as f:
+    template = f.read()
+
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+def extract_booking_info(user_input):
+    response = chain.invoke({"question": user_input})
+    raw = response.content if hasattr(response, 'content') else str(response)
+
+    match = re.search(r"\{[\s\S]*?\}", raw)
+    if not match:
+        print("❌ No JSON block found in response.")
+        print("Raw response:", raw)
+        return None
+
+    json_str = match.group(0)
+
+    try:
+        # data = json.loads(response.content if hasattr(response, 'content') else response)
+        data = json.loads(json_str)
+        return data
+    except Exception as e:
+        print(f"Error parsing model response: {e}")
+        print("Raw response:", response)
+        return None
